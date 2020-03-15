549b1e43d8ced39b4c6eba04204b8225ed8c1fb9
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-03-15 18:16:15 +0800

    - Added Calculator Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,18 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.black,
 
       body: SafeArea(
         child: Column(
-          children: <Widget>[
 
+          children: <Widget>[
+            
+            //  Number Container
             Row(
               children: <Widget>[
                 Expanded(
-                                  child: Container(
+                    child: Container(
                     margin: EdgeInsets.all(16.0),
                     padding: EdgeInsets.all(16.0),
                     color: Colors.white,
@@ -29,10 +31,199 @@ class Home extends StatelessWidget {
                       ),
                       ),
                   ),
+
                 ),
               ]
             ),
             
+            // Clear Button and Divider Button
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: <Widget> [
+                  Container(
+                    child: FlatButton(
+                      onPressed: () {},
+                      padding: EdgeInsets.all(16.0),
+                      color: Colors.grey[200],
+                      child: Text('C'),
+                      ),
+                  ),
+
+                  Container(
+                    child: FlatButton(
+                      onPressed: () {},
+                      padding: EdgeInsets.all(16.0),
+                      color: Colors.grey[200],
+                      child: Text('÷'),
+                      ),
+
+                  )
+                ]
+
+              ),
+            ),
+
+            //  Buttons 7,8,9, and Multiply Button
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: <Widget> [
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('7'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('8'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('9'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('×'),
+                  ),
+                ]
+
+              ),
+            ),
+
+            //  Buttons 4,5,6, and Subtract Button
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: <Widget> [
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('4'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('5'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('6'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('-'),
+                  ),
+                ]
+
+              ),
+            ),
+
+            //  Buttons 1,2,3, and Add Button
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: <Widget> [
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('1'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('2'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('3'),
+                  ),
+
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.grey[900],
+                    child: Text('+'),
+                  ),
+                ]
+
+              ),
+            ),
+
+            // Zero Button and Equal Button
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: <Widget> [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 4.0),
+                                          child: FlatButton(
+                        onPressed: () {},
+                        padding: EdgeInsets.all(16.0),
+                        color: Colors.grey[200],
+                        child: Text('0'),
+                        ),
+                    ),
+                  ),
+                  
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 4.0),
+                        child: FlatButton(
+                        onPressed: () {},
+                        padding: EdgeInsets.all(16.0),
+                        color: Colors.grey[200],
+                        child: Text('='),
+                        ),
+                    ),
+                  ),
+
+                ]
+
+              ),
+            ),
+
           ],
           
           ),
