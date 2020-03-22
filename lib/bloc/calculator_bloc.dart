04ec0d07ae1392d5471de7fe90bec1e8ed44f56d
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-03-22 11:57:30 +0800

    - Added Logic in the CalculatorBloc

--- lib/bloc/calculator_bloc.dart
+++ lib/bloc/calculator_bloc.dart
@@ -1,10 +1,56 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_calculator/enum/operation.dart';
+import 'package:simple_calculator/model/calculator.dart';
 
-class CalculatorBloc extends Bloc<Operation, int> {
+//  This will be our business logic class
+//  In the Bloc extension, we passed the model(Calculator)
+//  and the result(int)
+class CalculatorBloc extends Bloc<Calculator, int> {
 
   @override
   // TODO: implement initialState
   int get initialState => 0;
-  
+
+  @override
+  Stream<int> mapEventToState(Calculator event) {
+    // TODO: implement mapEventToState
+    
+    int result = 0;
+
+    switch(event.operation) {
+
+      case Operation.add:
+        result = event.firstNumber + event.secondNumber;
+
+        //  Update the initialState
+        yield result;
+        break;
+
+      case Operation.subtract:
+        result = event.firstNumber - event.secondNumber;
+
+        //  Update the initialState
+        yield result;
+        break;
+
+      case Operation.multiply:
+        result = event.firstNumber * event.secondNumber;
+
+        //  Update the initialState
+        yield result;
+        break;
+
+      case Operation.divide:
+        result = event.firstNumber ~/ event.secondNumber;
+
+        //  Update thhe initialState
+        yield result;
+        break;
+
+    }
+
+    return null;
+  }  
+
 }
---
+++ lib/model/calculator.dart
@@ -0,0 +1,11 @@
+
+import 'package:simple_calculator/enum/operation.dart';
+
+class Calculator {
+  final Operation operation;
+  final int firstNumber;
+  final int secondNumber;
+
+  Calculator(this.operation, this.firstNumber, this.secondNumber);
+  
+}
